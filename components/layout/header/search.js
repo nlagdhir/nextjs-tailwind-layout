b661dd758bbf412c95ca456c2d93bd046933ed40
Nilesh Lagdhir <nlagdhir@gmail.com>
2022-10-10 20:45:14 +0530

    simple layout created with tailwind

---
+++ components/layout/header/search.js
@@ -0,0 +1,7 @@
+const Search = () => {
+    return <>
+        <input type="search" name="search" id="search" placeholder="Search..." />
+    </>
+}
+
+export default Search;
---
+++ components/layout/introduction/index.js
@@ -0,0 +1,7 @@
+const Introduction = () => {
+    return <>
+        <h1>Introduction</h1>
+    </>
+}
+
+export default Introduction;
